5f279058cdf0a443df698e52dc72047ec9b1254f
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2026-02-01 02:35:06 +0000

    Add padding

--- app/components/top-bar.tsx
+++ app/components/top-bar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HeaderComponent() {
   return (
     <header>
-      <nav className="flex justify-between @container">
+      <nav className="flex justify-between @container p-4">
         <Link href="/">
           🐼 <span className="hidden @3xs:inline">Stash Panda Tag</span>
         </Link>
