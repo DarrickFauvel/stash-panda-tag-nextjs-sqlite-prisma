1181026672728e62678b39af0b2774862280f7f1
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2026-02-01 02:36:55 +0000

    Create hero component

---
+++ app/components/hero.tsx
@@ -0,0 +1,21 @@
+export default function HeroComponent() {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row">
+        <img
+          src="/images/stash-panda-tag-logo-lg.png"
+          className="max-w-full rounded-lg shadow-2xl"
+        />
+        <div>
+          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+}
