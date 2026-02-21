ea0cc04b70e49c10783387c8acf5584d816c1c75
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2026-02-21 23:17:11 +0000

    Fix alignment of items

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import IntroModalComponent from "./components/modal-intro";
 
 export default function Home() {
   return (
-    <main className="bg-zinc-50 font-sans dark:bg-black">
+    <main className="flex flex-col items-center justify-center gap-6 p-8 bg-zinc-50 font-sans dark:bg-black">
       <IntroModalComponent />
       <GenerateNewLabelCardComponent />
     </main>
