7fcd1aabb449054fe7527e13d873cf8b10ed231c
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2026-02-21 23:04:30 +0000

    Create a label page

---
+++ app/label/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function LabelPage() {
+  useEffect(() => {
+    // window.print();
+  });
+  return (
+    <article className="label">
+      <h1>A1-544-20260211-082101</h1>
+    </article>
+  );
+}
