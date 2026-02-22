5dcf2fbe547df32506860870489ef89e8678f7d5
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2026-02-22 03:03:02 +0000

    Fix light bg color

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import GenerateNewLabelCardComponent from "./components/card-generate-new-label"
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center gap-6 p-8 bg-zinc-50 font-sans dark:bg-black">
+    <main className="flex flex-col items-center justify-center gap-6 p-4 sm:p-8 bg-base-200 font-sans">
       <GenerateNewLabelCardComponent />
     </main>
   );
